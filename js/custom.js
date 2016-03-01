0aff8dca9b664d9ebf97c5f50822afcd4d6828eb
Erik Lanza <erik.lanzaa@gmail.com>
2016-03-01 16:14:00 +0200

    Fixed some syntax.

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
     $.getJSON('ErikLanza.json', function(data){
 
-        var eduData=data.education, workData = data.work_experience, skillsData=data.skills;
-        var $educationh2 = $('#education h2'), $work_experienceh2 = $('#work_experience h2'), $skillsh2 = $('#skills h2') ;
+        var educationData = data.education,
+        work_experienceData = data.work_experience,
+        skillsData = data.skills,
+        $educationh2 = $('#education h2'),
+        $work_experienceh2 = $('#work-experience h2'),
+        $skillsh2 = $('#skills h2');
 
-        $(eduData).each(function(index, value){
+        $(educationData).each(function(index, value){
             $educationh2.after('<p>' + value.school + '<br/>' + value.degree + '<br/> Completion: ' + value.percentage_completed + '%</p>');
         });
 
-        $(workData).each(function(index, value){
+        $(work_experienceData).each(function(index, value){
             $work_experienceh2.after('<span class="headings">' + value.timespan+'    '  +  value.place  + '   ' + value.country + '</span> <br/>' + value.task + '</p>');
         });
 
