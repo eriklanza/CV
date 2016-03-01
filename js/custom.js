4ddd032d0da08aba04a58fc36a78351acd319993
Erik Lanza <erik.lanzaa@gmail.com>
2016-03-01 17:42:17 +0200

    Added some styling.

--- js/custom.js
+++ js/custom.js
@@ -2,25 +2,25 @@ $(document).ready(function() {
     $.getJSON('ErikLanza.json', function(data){
 
         var educationData = data.education,
-        work_experienceData = data.work_experience,
+        workExperienceData = data.work_experience,
         skillsData = data.skills,
         $educationh2 = $('#education h2'),
-        $work_experienceh2 = $('#work-experience h2'),
+        $workExperienceh2 = $('#work-experience h2'),
         $skillsh2 = $('#skills h2');
 
         $(educationData).each(function(index, value){
             $educationh2.after('<p>' + value.school + '<br/>' + value.degree + '<br/> Completion: ' + value.percentage_completed + '%</p>');
         });
 
-        $(work_experienceData).each(function(index, value){
-            $work_experienceh2.after('<span class="headings">' + value.timespan+'    '  +  value.place  + '   ' + value.country + '</span> <br/>' + value.task + '</p>');
+        $(workExperienceData).each(function(index, value){
+            $workExperienceh2.after('<span class="headings">' + value.timespan+'    '  +  value.place  + '   ' + value.country + '</span> <br/>' + value.task + '</p>');
         });
 
         $(skillsData).each(function(index, value){
-            $skillsh2.after('<p>' + value.tool +' <span class="headings">   ' + value.rating + '</span></p>');
+            $skillsh2.after('<p class="normal">' + value.tool +' <span class="rating">   ' + value.rating + '</span></p>');
         });
 
-        $('#languages h2').after('<p>' + data.languages + '</p>');
+        $('#languages h2').after('<p>' + data.languages[0] + ' | ' + data.languages[1] + '</p>');
 
     });
 });
