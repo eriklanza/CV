580315a3dedca03d3584c3016f0fd449eb6178a0
Erik Lanza <erik.lanzaa@gmail.com>
2016-03-01 15:16:34 +0200

    Populated the HTML.

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
     $.getJSON('ErikLanza.json', function(data){
+
         $(data.education).each(function(index, value){
-            console.log(value.school);
+            $('#education h2').after('<p>' + value.school +'<br>' + value.degree +'<br> Completion: '+value.percentage_completed+ '%</p>');
         });
-        console.log(data.education);
+
+        $(data.work_experience).each(function(index, value){
+            $('#work_experience h2').after('<p class="headings">' + value.timespan+'    ' + value.place +'   '+ value.country+'</p> <p>'+value.task+ '</p>');
+        });
+
+        $(data.skills).each(function(index, value){
+            $('#skills h2').after('<p>' + value.tool+' <span class="headings">   ' + value.rating + '</span></p>');
+        });
+
+        $('#languages h2').after('<p>' + data.languages+'</p>');
+
+
+
     });
 });
