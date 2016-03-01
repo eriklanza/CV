5f4b3ad93beef27060fddc2da9637ce667e44636
Erik Lanza <erik.lanzaa@gmail.com>
2016-03-01 16:02:52 +0200

    Caching selectors.

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
     $.getJSON('ErikLanza.json', function(data){
 
-        $(data.education).each(function(index, value){
-            $('#education h2').after('<p>' + value.school +'<br>' + value.degree +'<br> Completion: '+value.percentage_completed+ '%</p>');
+        var eduData=data.education, workData = data.work_experience, skillsData=data.skills;
+        var $educationh2 = $('#education h2'), $work_experienceh2 = $('#work_experience h2'), $skillsh2 = $('#skills h2') ;
+
+        $(eduData).each(function(index, value){
+            $educationh2.after('<p>' + value.school + '<br/>' + value.degree + '<br/> Completion: ' + value.percentage_completed + '%</p>');
         });
 
-        $(data.work_experience).each(function(index, value){
-            $('#work_experience h2').after('<p class="headings">' + value.timespan+'    ' + value.place +'   '+ value.country+'</p> <p>'+value.task+ '</p>');
+        $(workData).each(function(index, value){
+            $work_experienceh2.after('<span class="headings">' + value.timespan+'    '  +  value.place  + '   ' + value.country + '</span> <br/>' + value.task + '</p>');
         });
 
-        $(data.skills).each(function(index, value){
-            $('#skills h2').after('<p>' + value.tool+' <span class="headings">   ' + value.rating + '</span></p>');
+        $(skillsData).each(function(index, value){
+            $skillsh2.after('<p>' + value.tool +' <span class="headings">   ' + value.rating + '</span></p>');
         });
 
-        $('#languages h2').after('<p>' + data.languages+'</p>');
-
-
+        $('#languages h2').after('<p>' + data.languages + '</p>');
 
     });
 });
