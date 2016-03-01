66fc5c00b0bf0f7c53159eebfdf4d350c80679c5
Erik Lanza <erik.lanzaa@gmail.com>
2016-03-01 19:54:06 +0200

    Layout changes done and stars rating added.

--- js/custom.js
+++ js/custom.js
@@ -6,21 +6,27 @@ $(document).ready(function() {
         skillsData = data.skills,
         $educationh2 = $('#education h2'),
         $workExperienceh2 = $('#work-experience h2'),
-        $skillsh2 = $('#skills h2');
+        $skillsDiv = $('#skills div');
 
         $(educationData).each(function(index, value){
             $educationh2.after('<p>' + value.school + '<br/>' + value.degree + '<br/> Completion: ' + value.percentage_completed + '%</p>');
         });
 
         $(workExperienceData).each(function(index, value){
-            $workExperienceh2.after('<span class="headings">' + value.timespan+'    '  +  value.place  + '   ' + value.country + '</span> <br/>' + value.task + '</p>');
+            $workExperienceh2.after('<span>'  +  value.place  + ' | ' + value.country + ' | ' + value.timespan + '</span> <br/>' + value.task + '</p>');
         });
 
         $(skillsData).each(function(index, value){
-            $skillsh2.after('<p class="normal">' + value.tool +' <span class="rating">   ' + value.rating + '</span></p>');
+            $skillsDiv.after('<div class="row"><div class="col-md-6 normal">' + value.tool +'</div> <div class="col-md-6 rating"> ' + stars(value.rating) + '</div></div>');
         });
 
         $('#languages h2').after('<p>' + data.languages[0] + ' | ' + data.languages[1] + '</p>');
+
+        function stars (num){
+            var number = parseInt(num);
+            var starNumber = '<i class="fa fa-star-o"></i>' .repeat(number);
+            return starNumber;
+        }
 
     });
 });
