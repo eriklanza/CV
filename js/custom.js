293d9b2cd189f854be50b327170a5d8a8d640182
Erik Lanza <erik.lanzaa@gmail.com>
2016-03-01 20:47:20 +0200

    Star rating finished.

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,36 @@
 $(document).ready(function() {
+    /*getting the information from the JSON file*/
     $.getJSON('ErikLanza.json', function(data){
-
+        /*caching*/
         var educationData = data.education,
         workExperienceData = data.work_experience,
         skillsData = data.skills,
         $educationh2 = $('#education h2'),
         $workExperienceh2 = $('#work-experience h2'),
         $skillsDiv = $('#skills div');
-
+        /*looping through the education object and getting the values required*/
         $(educationData).each(function(index, value){
             $educationh2.after('<p>' + value.school + '<br/>' + value.degree + '<br/> Completion: ' + value.percentage_completed + '%</p>');
         });
 
         $(workExperienceData).each(function(index, value){
-            $workExperienceh2.after('<span>'  +  value.place  + ' | ' + value.country + ' | ' + value.timespan + '</span> <br/>' + value.task + '</p>');
+            $workExperienceh2.after('<span class="work">'  +  value.place  + ' | ' + value.country + ' | ' + value.timespan + '</span> <br/>' + value.task + '</p>');
         });
 
         $(skillsData).each(function(index, value){
-            $skillsDiv.after('<div class="row"><div class="col-md-6 normal">' + value.tool +'</div> <div class="col-md-6 rating"> ' + stars(value.rating) + '</div></div>');
+            $skillsDiv.after('<div class="row"><div class="col-md-6 normal extra-margin">' + value.tool +'</div> <div class="col-md-6 rating"> ' + stars(value.rating) + '</div></div>');
         });
 
         $('#languages h2').after('<p>' + data.languages[0] + ' | ' + data.languages[1] + '</p>');
-
+        /* generating the star ratings*/
         function stars (num){
-            var number = parseInt(num);
-            var starNumber = '<i class="fa fa-star-o"></i>' .repeat(number);
-            return starNumber;
+            var number, remainingStars, starsNumber, rating;
+            number = parseInt(num);
+            remainder = 5-number;
+            starsNumber = '<i class="fa fa-star"></i>' .repeat(number);
+            remainingStars = '<i class="fa fa-star-o"></i>' .repeat(remainder);
+            rating = starsNumber + remainingStars;
+            return rating;
         }
 
     });
